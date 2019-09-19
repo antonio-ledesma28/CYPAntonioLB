7053dd1c0d92b7571906357e346ce3d2bccf32e1
antonio-ledesma28 <antoniolebr@gmail.com>
2019-09-19 17:29:29 -0500

    Ejercicios 1.1 a 1.3

---
+++ libro/problemas_resueltos/capitulo1/problema1_1.py
@@ -0,0 +1,4 @@
+PREPO=float(input("precio del producto:"))
+PAGO=float(input("De cuanto es tu pago:"))
+DEVO=PAGO-PREPO
+print(f"Pagaste:${PAGO}, el precio del producto es:${PREPO} y tu cambio es:${DEVO}")
---
+++ libro/problemas_resueltos/capitulo1/problema1_2.py
@@ -0,0 +1,4 @@
+BASE = float(input("Dame la base del triangulo:"))
+ALTU = float(input("Dame la base del triangulo:"))
+SUP = (BASE * ALTU)/2
+print(f"El area del triangulo es: {SUP}, si la base es: {BASE} y la altura es: {ALTU}")
