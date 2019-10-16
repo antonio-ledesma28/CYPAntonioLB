15e0ae06a7c449ef3f8ed88020f6301a922dcb0e
antonio-ledesma28 <antoniolebr@gmail.com>
2019-10-16 10:36:23 -0500

    Corrección de programas erroneos, tarea 2.

--- libro/problemas_resueltos/capitulo2/problema2_13.py
+++ libro/problemas_resueltos/capitulo2/problema2_13.py
@@ -1,7 +1,13 @@
 MAT = int(input("Escribe tu matrícula de alumno: "))
-CARR = str(input("Escribe la carrera a la que perteneces (en mayúsculas): "))
+CARR = input("Escribe la carrera a la que perteneces (en mayúsculas y sin acento): ")
 SEM = int(input("Escribe el número de semestre aprobado: "))
 PROM = float(input("Escribe tu promedio: "))
-if CARR == ECONOMIA :
-        if SEM >=6 and PROM >= 8.8 :
-            print (f"Tu matrícula es {MAT}, tu carrera es {CARR} y tú fuiste ACEPTADO")
+if CARR == 'ECONOMIA' :
+    if SEM >=6 and PROM >= 8.8 :
+        print (f"Tu matrícula es {MAT}, tu carrera es {CARR} y tú fuiste ACEPTADO")
+elif CARR == 'COMPUTACION' :
+    if SEM >6 and PROM > 8.5 :
+        print(f"Tu matrícula es {MAT} y tu carrera es {CARR}, FUISTE ACEPTADO")
+elif CARR == 'ADMINISTRACION' or CARR == 'CONTABILIDAD':
+    if SEM > 5 and PROM > 8.5 :
+        print(f"Tu matrícula es {MAT} y tu carrera es {CARR}, FUISTE ACEPTADO.")
--- libro/problemas_resueltos/capitulo2/problema2_9.py
+++ libro/problemas_resueltos/capitulo2/problema2_9.py
@@ -1,10 +1,10 @@
-prebas = float(input("Precio básico del producto"))
+prebas = float(input("Precio básico del producto: "))
 if prebas>500:
-    imp = 20*0.30+(prebas-40)*0.50
+    imp = (20*0.30)+(prebas-40)*(0.50)
 elif prebas > 40:
-    imp = 20*0.30+(prebas+40)*0.40
+    imp = (20*0.30)+(prebas-40)*(0.40)
 elif prebas > 20:
-    imp = (prebas-20)*0.30
+    imp = (prebas-20)*(0.30)
 else:
     imp = 0
 pretot= prebas+imp
