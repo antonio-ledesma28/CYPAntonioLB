37f0dd59885a32d5bfd203b06cd6f0f361c348b2
antonio-ledesma28 <antoniolebr@gmail.com>
2019-10-20 23:56:17 -0500

    creacion programas 3

---
+++ libro/problemas_resueltos/capitulo3/problema3_1.py
@@ -0,0 +1,13 @@
+SUMPAR= 0
+SUMIMP = 0
+CUERPO = 0
+for I in range (1,271,1):
+    NUM = (int(input("Ingresa el nùmero; "))
+            if NUM <> 0 :
+                if (-1**NUM)>0 :
+                    SUMPAR+= NUM
+                    CUEPAR+= 1
+                else :
+                    SUMIMP+= NUM
+PROPAR = SUMAR / CUEPAR
+print (f"PROPAR es {PROPAR} y SUMIMP ES {SUMIMP})
---
+++ libro/problemas_resueltos/capitulo3/problema3_2.py
@@ -0,0 +1,13 @@
+BAND = 'T'
+SUMSER = 0
+I = 2
+for I in range (2, 1801, 2):
+    SUMSER += 1
+    print (I)
+    if BAND = 'T':
+        BAND = 'F'
+        I += 3
+    else:
+        BAND = 'T'
+        I+=1
+print(f"SUMSER {SUMSER}")
